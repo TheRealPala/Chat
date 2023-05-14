09bd39e220708d07adb467f28d6bc4333f9f16f0
Alessandro Palano <apalano23@gmail.com>
2023-05-14 23:47:32 +0200

    FEAT:Added addUser method to UserRegistry

--- UserRegistry.cpp
+++ UserRegistry.cpp
@@ -45,12 +45,21 @@ std::vector<User>& UserRegistry::getAllUsers() const {
     std::fstream f(this->UserRegistryPath.c_str(), std::ios::in);
     std::vector<User> *users = new std::vector<User>();
     std::string line;
-    while(std::getline(f, line)){
-        if(line[0] == '#'){
+    while (std::getline(f, line)) {
+        if (line[0] == '#' || line.empty()) {
             continue;
         }
         std::vector<std::string> splittedLine = strExplode(line, '_');
         users->push_back(User(splittedLine[0], splittedLine[1], splittedLine[2], splittedLine[3], splittedLine[4]));
     }
     return *users;
+}
+bool UserRegistry::addUser(const User& user) const {
+    std::fstream f(this->UserRegistryPath.c_str(), std::ios::app);
+    if(!f.good()){
+        return false;
+    }
+    f << user.getId() << "_" << user.getName() << "_" << user.getSurname() << "_" << user.getMailBoxPath() << "_" << user.getCreatedAt() << "\n";
+    f.close();
+    return true;
 }
