d8b8e25a1225dd48105bda90456761f67e0cb144
Alessandro Palano <apalano23@gmail.com>
2023-05-14 23:50:01 +0200

    INFO:Updated old files of google-test framework

--- main.cpp
+++ main.cpp
@@ -3,11 +3,15 @@
 
 int main() {
     UserRegistry* userRegistration = new UserRegistry();
+    User u;
+    u.toString();
+    userRegistration->addUser(u);
     std::vector<User>& users = userRegistration->getAllUsers();
     for(auto u: users) {
         u.toString();
     }
     delete userRegistration;
     delete &users;
+
     return 0;
 }
