9557329f7839b0b1fa81e122a762ca0ffc7978ff
Alessandro Palano <apalano23@gmail.com>
2023-05-16 02:41:21 +0200

    INFO:Changed files to handle the new library

--- main.cpp
+++ main.cpp
@@ -1,17 +1,35 @@
 #include <iostream>
 #include "UserRegistry.h"
+#include "Chat.h"
+#include "millisecondTime.h"
 
 int main() {
+
     UserRegistry* userRegistration = new UserRegistry();
-    User u;
-    u.toString();
-    userRegistration->addUser(u);
-    std::vector<User>& users = userRegistration->getAllUsers();
+    User sender("sender", "sender");
+    User receiver("receiver", "receiver");
+    userRegistration->addUser(sender);
+    userRegistration->addUser(receiver);
+    std::vector<User> users = userRegistration->getAllUsers();
     for(auto u: users) {
         u.toString();
     }
-    delete userRegistration;
-    delete &users;
+    std::string pause = "100";
+    sender.sendMessage("Hi, how are you?", receiver);
+    system("sleep 0.2");
+    receiver.sendMessage("Fine, thanks", sender);
+    system("sleep 0.2");
+    receiver.sendMessage("And you?", sender);
+    system("sleep 0.2");
+    sender.sendMessage("Fine, thanks", receiver);
+    system("sleep 0.2");
+    sender.sendMessage("Bye", receiver);
+    system("sleep 0.2");
+    receiver.sendMessage("Bye", sender);
+    system("sleep 0.2");
+    Chat chat(sender, receiver);
+    chat.printChat();
+
 
     return 0;
 }
