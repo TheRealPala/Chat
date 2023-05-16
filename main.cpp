68482a047a6b1403e89ef50fd291f063b05c5de6
Alessandro Palano <apalano23@gmail.com>
2023-05-16 22:38:02 +0200

    INFO:Add autoUpdate files

--- main.cpp
+++ main.cpp
@@ -2,34 +2,25 @@
 #include "UserRegistry.h"
 #include "Chat.h"
 #include "millisecondTime.h"
+#include "CuiWrapper.h"
+#include "UserChatRegister.h"
 
 int main() {
-
-    UserRegistry* userRegistration = new UserRegistry();
-    User sender("sender", "sender");
-    User receiver("receiver", "receiver");
-    userRegistration->addUser(sender);
-    userRegistration->addUser(receiver);
-    std::vector<User> users = userRegistration->getAllUsers();
-    for(auto u: users) {
-        u.toString();
-    }
-    std::string pause = "100";
-    sender.sendMessage("Hi, how are you?", receiver);
-    system("sleep 0.2");
-    receiver.sendMessage("Fine, thanks", sender);
-    system("sleep 0.2");
-    receiver.sendMessage("And you?", sender);
-    system("sleep 0.2");
-    sender.sendMessage("Fine, thanks", receiver);
-    system("sleep 0.2");
-    sender.sendMessage("Bye", receiver);
-    system("sleep 0.2");
-    receiver.sendMessage("Bye", sender);
-    system("sleep 0.2");
-    Chat chat(sender, receiver);
+    UserRegistry userRegistry;
+    User currentUser = userRegistry.getUserByIndex(getUserIndex(userRegistry));
+    User reciever = userRegistry.getUserByIndex(getUserIndex(userRegistry, "Seleziona l'utente con cui chattare: "));
+    currentUser.sendMessage("ciao!", reciever);
+    system("sleep 0.3");
+    reciever.sendMessage("bella!", currentUser);
+    system("sleep 0.3");
+    currentUser.sendMessage("come va?", reciever);
+    Chat chat(currentUser, reciever);
+    std::cout << "Chat tra " << currentUser.getName() << " e " << reciever.getName() << std::endl;
     chat.printChat();
-
+    UserChatRegister currentUserRegister(currentUser);
+    Chat sentMessageToReciever = currentUserRegister.getMessagesSentWith(reciever);
+    std::cout << "Messaggi inviati a reciever" << std::endl;
+    sentMessageToReciever.printChat();
 
     return 0;
 }
