7ca5af672fd40dd0a07c597571d1763ce867adc9
Alessandro Palano <apalano23@gmail.com>
2023-05-17 21:21:20 +0200

    FEAT:Added main menu

--- main.cpp
+++ main.cpp
@@ -1,26 +1,37 @@
 #include <iostream>
 #include "UserRegistry.h"
 #include "Chat.h"
-#include "millisecondTime.h"
 #include "CuiWrapper.h"
 #include "UserChatRegister.h"
 
 int main() {
-    UserRegistry userRegistry;
-    User currentUser = userRegistry.getUserByIndex(getUserIndex(userRegistry));
-    User reciever = userRegistry.getUserByIndex(getUserIndex(userRegistry, "Seleziona l'utente con cui chattare: "));
-    currentUser.sendMessage("ciao!", reciever);
-    system("sleep 0.3");
-    reciever.sendMessage("bella!", currentUser);
-    system("sleep 0.3");
-    currentUser.sendMessage("come va?", reciever);
-    Chat chat(currentUser, reciever);
-    std::cout << "Chat tra " << currentUser.getName() << " e " << reciever.getName() << std::endl;
-    chat.printChat();
-    UserChatRegister currentUserRegister(currentUser);
-    Chat sentMessageToReciever = currentUserRegister.getMessagesSentWith(reciever);
-    std::cout << "Messaggi inviati a reciever" << std::endl;
-    sentMessageToReciever.printChat();
 
+    UserRegistry userRegistry;
+    bool loop;
+    do {
+        loop = true;
+        int choice = getMainMenuChoiceFromStdin();
+        switch (choice) {
+            case 1: {// aggiungi utente
+                addUser(userRegistry);
+            }
+            case 2: { // mesaggia
+                if(!userRegistry.enoughUsersToChat())
+                    std::cout << "Non ci sono utenti disponibili!\nAggiungi almeno due utenti per messaggiare!" << std::endl;
+                else
+                    subMenu(userRegistry);
+                break;
+            }
+            case 3: {// esci
+                loop = false;
+                break;
+            }
+            default: {
+                std::cout << "Scelta non valida!" << std::endl;
+                break;
+            }
+        }
+    } while (loop);
+    subMenu(userRegistry);
     return 0;
 }
