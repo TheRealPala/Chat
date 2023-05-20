1de48bc67ab17c8e44b8d928147f17120df1d7a5
Alessandro Palano <apalano23@gmail.com>
2023-05-20 03:01:28 +0200

    FIX: Fixed unremoved messageBox file

--- test/MessageFixture.cpp
+++ test/MessageFixture.cpp
@@ -8,16 +8,20 @@ class MessageSuite : public ::testing::Test {
 protected:
     virtual void SetUp() {
     }
-
-    Message mess;
 };
 
 
 TEST_F(MessageSuite, Costructor){
-
-
+    Message mess;
     ASSERT_EQ("alphanumericId", mess.getIdFrom());
     ASSERT_EQ("alphanumericId", mess.getIdTo());
     ASSERT_EQ("textFixture", mess.getText());
+}
+TEST_F(MessageSuite, hashFunction){
+    Message messA("125225", "258985", "text");
+    Message messB("125225", "258985", "text");
+    Message messC("895632", "745210", "provaTestoNonUguale");
+    ASSERT_EQ(messA.toHash(), messB.toHash()); //prova di uguaglianza Hash su messaggi uguali
+    ASSERT_NE(messA.toHash(), messC.toHash()); //prova di disuguaglianza Hash su messaggi non uguali
 }
 
--- test/UserRegistryFixture.cpp
+++ test/UserRegistryFixture.cpp
@@ -2,24 +2,20 @@
 
 #include "../User.h"
 #include "../UserRegistry.h"
-
+#include "../txtHandleFunctions.h"
 
 class UserRegistrySuite : public ::testing::Test {
 
 protected:
     virtual void SetUp() {
-        blankFile();
+        blankFile("config/userRegistry.txt", "#id_nome_cognome_pathMailBox_createdAt\n");
     }
     virtual void TearDown() {
-      blankFile();
-      deleteTxtFile(baseUser.getMailBoxPath());
+      blankFile("config/userRegistry.txt", "#id_nome_cognome_pathMailBox_createdAt\n");
+      baseUser.getMessBox().deleteMessageBox();
+      readUser.getMessBox().deleteMessageBox();
     }
-    void blankFile(){
-        std::string path = "config/userRegistry.txt";
-        std::ofstream f(path);
-        f << "#id_nome_cognome_pathMailBox_createdAt\n";
-        f.close();
-    }
+
     void deleteTxtFile(std::string path){
         std::remove(path.c_str());
     }
@@ -31,6 +27,7 @@ protected:
 
 TEST_F(UserRegistrySuite, checkDataReadUser){
     registry.addUser(baseUser);
+    readUser.getMessBox().deleteMessageBox();
     readUser = registry.getAllUsers().at(0);
     ASSERT_EQ(baseUser.getId(), readUser.getId());
     ASSERT_EQ(baseUser.getName(), readUser.getName());
